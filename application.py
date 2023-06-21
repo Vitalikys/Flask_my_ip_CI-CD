80cccb5463e23cb2ff3ad1386eab60ba5699e233
vitalikys <vitalikys87@gmail.com>
2023-06-21 14:07:53 +0300

    app ver 2

--- application.py
+++ application.py
@@ -1,8 +1,21 @@
 import requests
 from flask import Flask, render_template
-import socket  # for IP address
+import socket      # for IP address
+import subprocess  # run linux bash cmd
 
 app = Flask(__name__)
+
+
+def get_hostmname_I():
+    """
+    get IP from linux bash. Using subprocess module
+    :return: Exception text or IP
+    """
+    try:
+        command = 'hostname -I'
+        return subprocess.check_output(command, shell=True).decode('utf-8')
+    except Exception as e:
+        return f"Got an exception when calling 'hostname -I'; \n {str(e)}"
 
 
 @app.route('/')
@@ -13,10 +26,13 @@ def index():
         # ip_identme = ip_address.decode('utf-8'))
         ip = requests.get("http://checkip.amazonaws.com/").text
         print(f"Public IP address: {ip_address}")
+
+        # get IP from linux bash
+        ip_host = get_hostmname_I()
     except Exception as e:
         return str(e)
 
-    return render_template('index.html', ip_identme=ip_identme, ip=ip)
+    return render_template('index.html', ip_identme=ip_identme, ip=ip, ip_host=ip_host)
 
 
 if __name__ == "__main__":
---
+++ dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.9-slim
+
+WORKDIR /code
+
+COPY . /code
+
+COPY ./requirements.txt .
+
+RUN pip install -r requirements.txt
+CMD [ "python3",  "wsgi.py"]
